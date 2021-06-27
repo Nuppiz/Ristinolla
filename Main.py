46c5e6cc2ac32f64c3359be1428150244b90747c
Nuppiz <nupethenuke@hotmail.com>
2021-06-27 19:31:00 +0300

    It's still broken

--- Main.py
+++ Main.py
@@ -66,12 +66,13 @@ def player_input(board):
 		board[player_row][player_col] = "X"
 
 # function loop to keep the game going, checks after each turn for score and possible draw situation
-def game_loop(board, difficulty, win_score):
+def game_loop(board, difficulty, win_score, window, renderer, processor):
 	while True:
 		print ("Your turn")
 		player_input(board)
 		Board.print_board(board)
-		visual_feedback(board)
+		processor.run(window)
+		visual_feedback(board, renderer)
 		if Board.score_checker(board, 'X', win_score) == 1:
 			print ("You win!")
 			break
@@ -80,6 +81,8 @@ def game_loop(board, difficulty, win_score):
 			
 		print ("AI's turn...")
 		sleep(2) # 2-second delay to make it seem like the AI is "thinking"
+		processor.run(window)
+		visual_feedback(board, renderer)
 		Ai.ai_input(board, difficulty, win_score)
 		Board.print_board(board)
 		if Board.score_checker(board, 'O', win_score) == 1:
@@ -90,12 +93,27 @@ def game_loop(board, difficulty, win_score):
 
 # game initialization function and exit text	
 def main():
+	# initialize  SDL2 objects & variables
+	sdl2.ext.init()
+	width = 640
+	height = 480
+	window = sdl2.ext.Window("Hello World!", size=(width, height))      
+	renderer = sdl2.ext.Renderer(window)
+	processor = sdl2.ext.TestEventProcessor()
+	# show window
+	window.show()
+	
 	game_board = []
 	print ("Welcome to Tic-Tack-Toes")
 	win_score = Board.init_board(game_board, "-")
 	difficulty = diff_check() * 5 # defines difficulty variable which is sent to AI
+	
+	processor.run(window)
+	visual_feedback(board, renderer)
 	Board.print_board(game_board)
-	game_loop(game_board, difficulty, win_score)
+	
+	game_loop(game_board, difficulty, win_score, window, renderer, processor)
+	
 	print ("Game over man, game over!")
 	new_game = input("Enter Y to play again, any other key to quit:")
 	if new_game == "Y" or new_game == "y":
@@ -117,7 +135,7 @@ def draw_cross(x, y):
 	draw_rectangle(x, y+17, cross_size_x, cross_size_y, cross_col_r, cross_col_g, cross_col_b)
 	draw_rectangle(x+17, y, cross_size_y, cross_size_x, cross_col_r, cross_col_g, cross_col_b)
 
-def visual_feedback(board):
+def visual_feedback(board, renderer):
 	draw_x = 0
 	draw_y = 0
 	for row in range(0,len(board)):
@@ -127,16 +145,6 @@ def visual_feedback(board):
 				draw_y = int(row)*42
 				draw_cross(draw_x, draw_y)
 	renderer.present()
-	
-sdl2.ext.init()
-width = 640
-height = 480
-window = sdl2.ext.Window("Hello World!", size=(width, height))			
-window.show()
-renderer = sdl2.ext.Renderer(window)
-	
-processor = sdl2.ext.TestEventProcessor()
-processor.run(window)
 	
 # execution actually begins here
 main()
