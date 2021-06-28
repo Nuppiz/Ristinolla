125cc5cdad65b1c6459bd3e7bca8b41c03848e49
Nuppiz <nupethenuke@hotmail.com>
2021-06-28 17:58:23 +0300

    Current version, ai_think needs major revision

--- Ai.py
+++ Ai.py
@@ -51,42 +51,49 @@ def ai_prevent_player_win(board, difficulty, win_score):
       return True
     # if out of attempts, end this function
     elif attempts >= max_attempts:
+      board[ai_y][ai_x] = "-"
       return False
     # else erase the temporary X and try again
     else:
       board[ai_y][ai_x] = "-"
   
-def ai_think(board, difficulty, ai_max):
-    attempts = 0
-    max_attempts = len(board[0]) * len(board) * difficulty # determines max attempts by board size and difficulty
-    while True:
-        # randint to generate a random pair of coordinates
-        ai_y = randint(0, len(board) - 1)
-        ai_x = randint(0, len(board[0]) - 1)
-        # checks that the cell is empty, if not, try again with new coords
-        if board[ai_y][ai_x] != "-":
-            continue
-        else:
-            board[ai_y][ai_x] = "O"
-            attempts += 1
-        if Board.getMax(board, 'O') > ai_max or attempts >= max_attempts:
-            break
-        else:
-            board[ai_y][ai_x] = "-"
+def ai_think(board, difficulty):
+  attempts = 0
+  max_attempts = len(board[0]) * len(board) * difficulty # determines max attempts by board size and difficulty
+  ai_max = Board.getMax(board, 'O')
+
+  while True:
+    # randint to generate a random pair of coordinates
+    ai_y = randint(0, len(board) - 1)
+    ai_x = randint(0, len(board[0]) - 1)
+
+    # checks that the cell is empty, if not, try again with new coords
+    if board[ai_y][ai_x] != "-":
+        continue
+    else:
+        board[ai_y][ai_x] = "O"
+        attempts += 1
+
+    # once an O has been placed above, check if we get a longer straight or if we ran out of attempts
+    if Board.getMax(board, 'O') > ai_max or attempts >= max_attempts:
+        break
+    # if above not true and attempts still left, erase O and try again
+    else:
+        board[ai_y][ai_x] = "-"
             
 def ai_input(board, difficulty, win_score):
-    ai_max = Board.getMax(board, 'O')
-    player_max = Board.getMax(board, 'X')
-    success = False
-    
-    # check if almost winning, and try to place last O
-    if ai_max == win_score-1:
-          success = ai_try_win(board, difficulty, win_score)
-    
-    # check if player is almost winning
-    if not success and player_max == win_score-1:
-          success = ai_prevent_player_win(board, difficulty, win_score)
+  success = False
+  
+  # check if almost winning, and try to place last O
+  print("AI tries to win")
+  success = ai_try_win(board, difficulty, win_score)
+  
+  # check if player is almost winning
+  if not success:
+    print("AI didn't win, try to stop player win")
+    success = ai_prevent_player_win(board, difficulty, win_score)
 
-    # finally, just try to add an O somewhere
-    if not success:
-          ai_think(board, difficulty, ai_max)
+  # finally, just try to add an O somewhere
+  if not success:
+    print("Didn't see player winning anywhere or couldn't stop him, just think normally")
+    ai_think(board, difficulty)
